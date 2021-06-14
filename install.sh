6ce91adc66f60091f205993d7c5ab9035340a3b0
ds1605 <danielcoles1605@gmail.com>
2021-06-14 19:14:18 +0800

    Changes...

--- install.sh
+++ install.sh
@@ -32,31 +32,43 @@ function link_install() {
 
 ### Installation
 ## Shell and Terms
+# Base
 link_install "${DIR_BASE}/shell/profile" "${DIR_INSTALL}/.profile"
 for i in {antigen.zsh,pastefix.zsh,powerline.zsh,zshrc}
 do
 	link_install "${DIR_BASE}/shell/zsh/${i}" "${DIR_INSTALL}/.${i}"
 done
 link_install "${DIR_BASE}/tmux/tmux.conf" "${DIR_INSTALL}/.tmux.conf"
+
 # Kitty
 for i in {kitty.conf,theme.conf}
 do
 	link_install "${DIR_BASE}/config/kitty/${i}" "${DIR_INSTALL}/.config/kitty/${i}"
 done
+
 # Termite
 for i in config
 do
     link_install "${DIR_BASE}/config/termite/${i}" "${DIR_INSTALL}/.config/termite/${i}"
 done
 
+
 ## Desktop
 # Compositor
 link_install "${DIR_BASE}/config/picom/picom.conf" "${DIR_INSTALL}/.config/picom/picom.conf"
+
 # Awesome
 link_install "${DIR_BASE}/desktop/awesome" "${DIR_INSTALL}/.config/awesome"
 
 ## Editors
 # Emacs, but doom
-for i in {config.el,custom.el,init,el,packages.el} do
-	link_install "${DIR_BASE}/emacs/doom.d/${i}" "${DIR_INSTALL}/.doom.d/${i}"
+# for i in {config.el,custom.el,init.el,packages.el}
+# do
+# 	link_install "${DIR_BASE}/emacs/doom.d/${i}" "${DIR_INSTALL}/.doom.d/${i}"
+# done
+
+# Vim
+for i in vimrc
+do
+	link_install "${DIR_BASE}/config/vim/${i}" "${DIR_INSTALL}/.${i}"
 done
